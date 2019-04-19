026a541d42054861908b00b8a0fd3891cb3af0cc
Tech-a41 <tech-a41@Tech-A41s-MacBook-Air.local>
2019-04-19 12:04:22 +0100

    Arrow keys logged to the console successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log("Loaded Page");
+
+$(function(){
+
+  //Event Listener for keyboard presses
+  $(document).keyup(function(event) {
+
+        switch (event.key) {
+          case "ArrowUp":
+            console.log("Up Key Pressed");
+            break;
+          case "ArrowDown":
+            console.log("Down Key Pressed");
+            break;
+          case "ArrowRight":
+            console.log("Right Key Pressed");
+            break;
+          case "ArrowLeft":
+            console.log("Left Key Pressed");
+            break;
+          default:
+            console.log(event.key);
+        }
+    });
+
+});
