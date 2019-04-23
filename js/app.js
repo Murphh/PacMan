872d310f55b154b6f34142c4aeebd9a9766e0345
Tech-a41 <tech-a41@Tech-A41s-MacBook-Air.local>
2019-04-23 10:00:55 +0100

    Added functionality for populating screen with coins on page load

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [1, 6]
   };
   decisionSquares[1] = {
     leftPosition: 125,
@@ -15,7 +16,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [0, 2, 7]
   }
   decisionSquares[2] = {
     leftPosition: 245,
@@ -23,7 +25,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [1, 9]
   }
   decisionSquares[3] = {
     leftPosition: 305,
@@ -31,7 +34,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [4, 10]
   }
   decisionSquares[4] = {
     leftPosition: 425,
@@ -39,7 +43,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [3, 5, 12]
   }
   decisionSquares[5] = {
     leftPosition: 525,
@@ -47,7 +52,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [4, 13]
   }
   decisionSquares[6] = {
     leftPosition: 15,
@@ -55,7 +61,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [0, 7, 14]
   }
   decisionSquares[7] = {
     leftPosition: 125,
@@ -63,7 +70,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [1, 6, 8, 15]
   }
   decisionSquares[8] = {
     leftPosition: 185,
@@ -71,7 +79,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [7, 9, 16]
   }
   decisionSquares[9] = {
     leftPosition: 245,
@@ -79,7 +88,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [2, 8, 10]
   }
   decisionSquares[10] = {
     leftPosition: 305,
@@ -87,7 +97,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [3, 9, 11]
   }
   decisionSquares[11] = {
     leftPosition: 365,
@@ -95,7 +106,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [10, 12, 19]
   }
   decisionSquares[12] = {
     leftPosition: 425,
@@ -103,7 +115,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [4, 11, 13]
   }
   decisionSquares[13] = {
     leftPosition: 525,
@@ -111,7 +124,8 @@ $(function(){
     up: true,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [5, 12]
   }
   decisionSquares[14] = {
     leftPosition: 15,
@@ -119,7 +133,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [6, 15]
   }
   decisionSquares[15] = {
     leftPosition: 125,
@@ -127,7 +142,8 @@ $(function(){
     up: true,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [7, 14, 26]
   }
   decisionSquares[16] = {
     leftPosition: 185,
@@ -135,7 +151,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [8, 17]
   }
   decisionSquares[17] = {
     leftPosition: 245,
@@ -143,7 +160,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [16, 23]
   }
   decisionSquares[18] = {
     leftPosition: 305,
@@ -151,7 +169,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [19, 24]
   }
   decisionSquares[19] = {
     leftPosition: 365,
@@ -159,7 +178,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [11, 18]
   }
   decisionSquares[20] = {
     leftPosition: 425,
@@ -167,7 +187,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [12, 21, 29]
   }
   decisionSquares[21] = {
     leftPosition: 525,
@@ -175,7 +196,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [13, 20]
   }
   decisionSquares[22] = {
     leftPosition: 185,
@@ -183,7 +205,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [23, 27]
   }
   decisionSquares[23] = {
     leftPosition: 245,
@@ -191,7 +214,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [17, 22, 24]
   }
   decisionSquares[24] = {
     leftPosition: 305,
@@ -199,7 +223,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [18, 23, 25]
   }
   decisionSquares[25] = {
     leftPosition: 365,
@@ -207,7 +232,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [24, 28]
   }
   decisionSquares[26] = {
     leftPosition: 125,
@@ -215,7 +241,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [15, 27, 33]
   }
   decisionSquares[27] = {
     leftPosition: 185,
@@ -223,7 +250,8 @@ $(function(){
     up: true,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [22, 26, 30]
   }
   decisionSquares[28] = {
     leftPosition: 365,
@@ -231,7 +259,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [25, 29, 31]
   }
   decisionSquares[29] = {
     leftPosition: 425,
@@ -239,7 +268,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [20, 28, 38]
   }
   decisionSquares[30] = {
     leftPosition: 185,
@@ -247,7 +277,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [27, 31, 34]
   }
   decisionSquares[31] = {
     leftPosition: 365,
@@ -255,7 +286,8 @@ $(function(){
     up: true,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [28, 30, 37]
   }
   decisionSquares[32] = {
     leftPosition: 15,
@@ -263,7 +295,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [33, 40]
   }
   decisionSquares[33] = {
     leftPosition: 125,
@@ -271,7 +304,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [26, 32, 34, 42]
   }
   decisionSquares[34] = {
     leftPosition: 185,
@@ -279,7 +313,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [30, 33, 35]
   }
   decisionSquares[35] = {
     leftPosition: 245,
@@ -287,7 +322,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [34, 44]
   }
   decisionSquares[36] = {
     leftPosition: 305,
@@ -295,7 +331,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [37, 45]
   }
   decisionSquares[37] = {
     leftPosition: 365,
@@ -303,7 +340,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [31, 36, 38]
   }
   decisionSquares[38] = {
     leftPosition: 425,
@@ -311,7 +349,8 @@ $(function(){
     up: true,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [29, 37, 39, 47]
   }
   decisionSquares[39] = {
     leftPosition: 525,
@@ -319,7 +358,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [38, 49]
   }
   decisionSquares[40] = {
     leftPosition: 15,
@@ -327,7 +367,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [32, 41]
   }
   decisionSquares[41] = {
     leftPosition: 65,
@@ -335,7 +376,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [40, 51]
   }
   decisionSquares[42] = {
     leftPosition: 125,
@@ -343,7 +385,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [33, 43, 52]
   }
   decisionSquares[43] = {
     leftPosition: 185,
@@ -351,7 +394,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [42, 44, 53]
   }
   decisionSquares[44] = {
     leftPosition: 245,
@@ -359,7 +403,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [35, 43, 45]
   }
   decisionSquares[45] = {
     leftPosition: 305,
@@ -367,7 +412,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [36, 44, 46]
   }
   decisionSquares[46] = {
     leftPosition: 365,
@@ -375,7 +421,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [45, 47, 56]
   }
   decisionSquares[47] = {
     leftPosition: 425,
@@ -383,7 +430,8 @@ $(function(){
     up: true,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [38, 46, 57]
   }
   decisionSquares[48] = {
     leftPosition: 485,
@@ -391,7 +439,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [49, 58]
   }
   decisionSquares[49] = {
     leftPosition: 525,
@@ -399,7 +448,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [39, 48]
   }
   decisionSquares[50] = {
     leftPosition: 15,
@@ -407,7 +457,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [51, 60]
   }
   decisionSquares[51] = {
     leftPosition: 65,
@@ -415,7 +466,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [41, 50]
   }
   decisionSquares[52] = {
     leftPosition: 125,
@@ -423,7 +475,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [42, 51]
   }
   decisionSquares[53] = {
     leftPosition: 185,
@@ -431,7 +484,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [43, 54]
   }
   decisionSquares[54] = {
     leftPosition: 245,
@@ -439,7 +493,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [53, 61]
   }
   decisionSquares[55] = {
     leftPosition: 305,
@@ -447,7 +502,8 @@ $(function(){
     up: false,
     right: true,
     down: true,
-    left: false
+    left: false,
+    connectedSquares: [56, 62]
   }
   decisionSquares[56] = {
     leftPosition: 365,
@@ -455,7 +511,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [46, 55]
   }
   decisionSquares[57] = {
     leftPosition: 425,
@@ -463,7 +520,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [47, 58]
   }
   decisionSquares[58] = {
     leftPosition: 485,
@@ -471,7 +529,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [48, 57, 59]
   }
   decisionSquares[59] = {
     leftPosition: 525,
@@ -479,7 +538,8 @@ $(function(){
     up: false,
     right: false,
     down: true,
-    left: true
+    left: true,
+    connectedSquares: [58, 63]
   }
   decisionSquares[60] = {
     leftPosition: 15,
@@ -487,7 +547,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: false
+    left: false,
+    connectedSquares: [50, 61]
   }
   decisionSquares[61] = {
     leftPosition: 245,
@@ -495,7 +556,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [54, 60, 62]
   }
   decisionSquares[62] = {
     leftPosition: 305,
@@ -503,7 +565,8 @@ $(function(){
     up: true,
     right: true,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [55, 61, 63]
   }
   decisionSquares[63] = {
     leftPosition: 525,
@@ -511,7 +574,8 @@ $(function(){
     up: true,
     right: false,
     down: false,
-    left: true
+    left: true,
+    connectedSquares: [59, 62]
   }
 
   //var for setting the distance PacMan moves every interval
@@ -527,6 +591,10 @@ $(function(){
   var pacManTop = $('#pacman').position().top;
   var pacManRight = pacManLeft + pacManWidth;
   var pacManBottom = pacManTop + pacManHeight;
+
+
+  //populate the screen with coins on page load
+  addCoins();
 
   //Event Listener that stop default action of spacebar being pressed
   $(document).keydown(function(event) {
@@ -659,6 +727,62 @@ $(function(){
     }
 
     return previousOpt;
+  }
+
+  //function for adding coins to the screen
+  function addCoins(){
+
+    //The number of squares that we will looping through
+    var numberOfSquares = decisionSquares.length;
+    //loop through all the squares
+    for (var y = 0; y < numberOfSquares; y++) {
+      //current square
+      var startSquare = decisionSquares[y];
+      var numOfConnectedSquares = startSquare.connectedSquares.length;
+      //loop through each square connected to the current square
+      for (var x = 0; x < numOfConnectedSquares; x++) {
+        //if the current square is horizontal to its connected square
+        if(startSquare.topPosition == decisionSquares[startSquare.connectedSquares[x]].topPosition){
+          //the values the coins will go between
+          var startLeft = startSquare.leftPosition + 12.5;
+          var startTop = startSquare.topPosition + 12.5;
+          var endLeft = decisionSquares[startSquare.connectedSquares[x]].leftPosition + 12.5;
+
+          //if moving in the positive direction
+          if(endLeft > startLeft){
+            for (var i = startLeft; i <= endLeft; i+=10) {
+              $(".game-area").append('<img src="img/coin.png" class="coin-icon" style="left:' + i + 'px; top: ' + startTop + 'px">');
+            }
+          }
+          //if moving in the negative direction
+          else{
+            for (var i = startLeft; i >= endLeft; i-=10) {
+              $(".game-area").append('<img src="img/coin.png" class="coin-icon" style="left:' + i + 'px; top: ' + startTop + 'px">');
+            }
+          }
+
+        }
+        //if the current square is vertical from its connected square
+        if (startSquare.leftPosition == decisionSquares[startSquare.connectedSquares[x]].leftPosition) {
+          //the values the coins will go between
+          var startLeft = startSquare.leftPosition + 12.5;
+          var startTop = startSquare.topPosition + 12.5;
+          var endTop = decisionSquares[startSquare.connectedSquares[x]].topPosition + 12.5;
+          //if moving in the negative direction
+          if (startTop > endTop) {
+            for (var i = startTop; i >= endTop; i-=10) {
+              $(".game-area").append('<img src="img/coin.png" class="coin-icon" style="left:' + startLeft + 'px; top: ' + i + 'px">');
+            }
+          }
+          //if moving in the positive direction
+          else{
+            for (var i = startTop; i <= endTop; i+=10) {
+              $(".game-area").append('<img src="img/coin.png" class="coin-icon" style="left:' + startLeft + 'px; top: ' + i + 'px">');
+            }
+          }
+        }
+      }
+    }
   }
 
 });
